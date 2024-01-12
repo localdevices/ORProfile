8e90c97b347452f5558daf2a7660d593a53b21bc
hcwinsemius <h.c.winsemius@gmail.com>
2024-01-12 18:07:59 +0100

    test for depth_2d #6

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -6,9 +6,10 @@ import cartopy.crs as ccrs
 import matplotlib
 
 def test_get_dist(mesh):
-    da = mesh.map_rowwise(
+    da = mesh.map_rowcol_wise(
         orprofile.profile.get_dist,
-        name="distance from bank"
+        name="y",
+        rowcol="cols"
     )
     da.ugrid.plot()
     plt.show()
@@ -16,11 +17,11 @@ def test_get_dist(mesh):
 
 def test_get_depth(mesh_high_res):
     ax = mesh_high_res.plot()
-    da = mesh_high_res.map_rowwise(
+    da = mesh_high_res.map_rowcol_wise(
         orprofile.profile.get_depth,
         name="depth from bank",
-        phi_left=0.1*np.pi,
-        phi_right=0.1*np.pi
+        phi_left=0.15*np.pi,
+        phi_right=0.005*np.pi
     )
     da_proj = da.ugrid.to_crs(ax.projection)
     da_proj.ugrid.plot(ax=ax, vmax=0., alpha=0.4, cmap="viridis", zorder=3)  #ax=ax, transform=ccrs.epsg(mesh_high_res.splines.crs.to_epsg()),
